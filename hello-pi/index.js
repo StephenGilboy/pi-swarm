1928e2af887800e7f9b7ea8cf825078bcf2b7065
Stephen Gilboy <sgilboy@outlook.com>
2017-11-05 19:24:56 -0800

    Use hello-pi for name

--- hello-pi/index.js
+++ hello-pi/index.js
@@ -3,5 +3,5 @@ const os = require('os');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(`<h1>I'm ${os.hostname()}</h1>`);
+  res.end(`<h1>Hello, Pi! I'm ${os.hostname()}</h1>`);
 }).listen(8000);
